7ffea3bb03562d8d5d72c3229395c97c8095dcbb
Arpit078 <verma.arpit078@.com>
2022-02-28 00:20:07 +0530

    Added arrow key functions

--- script.js
+++ script.js
@@ -14,10 +14,10 @@ let lastRenderTime = 0
 let snakeArr = [
     { x: 13, y: 15 }
 ]
-let score = 0 
-let a = 2 
+let score = 0
+let a = 2
 let b = 22
-let food = {x: Math.round(a + (b-a)*Math.random()),y: Math.round(a + (b-a)*Math.random()) }
+let food = { x: Math.round(a + (b - a) * Math.random()), y: Math.round(a + (b - a) * Math.random()) }
 
 
 //game functions
@@ -30,49 +30,49 @@ function fps(current_time) {
     gameEngine()
 
 }
-function collided(snake){
-    if(snake[0].x >= 25 || snake[0].x <=0 || snake[0].y >= 25 || snake[0].y <=0){
-        
+function collided(snake) {
+    if (snake[0].x >= 25 || snake[0].x <= 0 || snake[0].y >= 25 || snake[0].y <= 0) {
+
         return true;
     }
 
 }
 function gameEngine() {
-    if(collided(snakeArr)){
+    if (collided(snakeArr)) {
         gameoverSound.play()
-        inputDir = {x:0 , y: 0}
-        
+        inputDir = { x: 0, y: 0 }
+
         alert("Game Over, press space bar to restart!")
         snakeArr = [{ x: 13, y: 15 }]
         score = 0
         scorebox.innerHTML = "Score: " + score
 
-        
+
 
     }
-    if(snakeArr[0].y === food.y && snakeArr[0].x === food.x){
-        snakeArr.unshift({x : snakeArr[0].x + inputDir.x , y: snakeArr[0].y + inputDir.y})
+    if (snakeArr[0].y === food.y && snakeArr[0].x === food.x) {
+        snakeArr.unshift({ x: snakeArr[0].x + inputDir.x, y: snakeArr[0].y + inputDir.y })
         foodSound.play()
         score += 1
-        if(score>hiscorevalue){
+        if (score > hiscorevalue) {
             hiscorevalue = score
             localStorage.setItem("hiscore", JSON.stringify(hiscorevalue));
             highscorebox.innerHTML = "High Score: " + hiscorevalue
         }
         scorebox.innerHTML = "Score: " + score;
-        let a = 2 
+        let a = 2
         let b = 22
-        food = {x: Math.round(a + (b-a)*Math.random()),y: Math.round(a + (b-a)*Math.random()) }
+        food = { x: Math.round(a + (b - a) * Math.random()), y: Math.round(a + (b - a) * Math.random()) }
     }
     //for snake movement
-    for (let i = snakeArr.length - 2; i>=0; i--) { 
-        snakeArr[i+1] = {...snakeArr[i]};
+    for (let i = snakeArr.length - 2; i >= 0; i--) {
+        snakeArr[i + 1] = { ...snakeArr[i] };
     }
 
     snakeArr[0].x += inputDir.x;
     snakeArr[0].y += inputDir.y;
-    
-    
+
+
     board.innerHTML = ""
     snakeArr.forEach((e, index) => {
         const snakeElement = document.createElement('div')
@@ -98,11 +98,11 @@ function gameEngine() {
 
 //main logic 
 let hiscore = localStorage.getItem("hiscore")
-if(hiscore === null){
+if (hiscore === null) {
     hiscorevalue = 0
     localStorage.setItem("hiscore", JSON.stringify(hiscorevalue))
 }
-else{
+else {
     hiscorevalue = JSON.parse(hiscore);
     highscorebox.innerHTML = "High Score: " + hiscore;
 }
@@ -112,25 +112,46 @@ window.addEventListener("keydown", e => {
     moveSound.play()
     switch (e.key) {
         case "w":
-            
+
+            inputDir.x = 0
+            inputDir.y = -1
+            break;
+        case "ArrowUp":
+
             inputDir.x = 0
             inputDir.y = -1
             break;
         case "s":
-            
-            inputDir.x= 0
-            inputDir.y= +1
+
+            inputDir.x = 0
+            inputDir.y = +1
+            break
+        case "ArrowDown":
+
+            inputDir.x = 0
+            inputDir.y = +1
             break
         case "a":
-            
-            inputDir.x= -1
-            inputDir.y= 0
+
+            inputDir.x = -1
+            inputDir.y = 0
+            break
+        case "ArrowLeft":
+
+            inputDir.x = -1
+            inputDir.y = 0
             break
         case "d":
-            
-            inputDir.x= +1
-            inputDir.y= 0
-            
+
+            inputDir.x = +1
+            inputDir.y = 0
+
+            break
+        case "ArrowRight":
+
+            inputDir.x = +1
+            inputDir.y = 0
+
             break
         default:
             break;
